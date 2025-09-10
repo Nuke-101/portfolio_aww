9ccbbb58d51b0d057d0592462bf831c1821db61f
Nikhil Shinde <nikebuzz101@gmail.com>
2025-09-10 22:39:22 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import LoadingScreen from "./components/LoadingScreen.vue";
+import HomePage from "./components/HomePage.vue";
+</script>
+
+<template>
+  <HomePage></HomePage>
+  <LoadingScreen></LoadingScreen>
+</template>
